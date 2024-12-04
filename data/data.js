6fd870498872b0356633247fac3cd00a7c825f89
Louison <sillon01@protonmail.com>
2024-12-04 11:15:00 -0500

    End project

---
+++ data/data.js
@@ -0,0 +1,17 @@
+const dataObjects = [
+    {
+        title: "Vue.js",
+        desc: "Vue.js is an open-source model–view–viewmodel JavaScript framework for building user interfaces and single-page applications.",
+        img: "https://vuejs.org/images/logo.png"
+    },
+    {
+        title: "React",
+        desc: "React is an open-source, front end, JavaScript library for building user interfaces or UI components.",
+        img: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+    },
+    {
+        title: "Angular",
+        desc: "Angular is a platform for building mobile and desktop web applications using TypeScript/JavaScript and other languages.",
+        img: "https://angular.io/assets/images/logos/angular/angular.png"
+    }
+];
